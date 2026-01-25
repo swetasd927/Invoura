b456fdec84269983476613c69cd7e2920c2181f3
Sweta Dahal <dahalsweta5@gmail.com>
2026-01-26 00:18:08 +0545

    backend setup

---
+++ backend/index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config'
+import { clerkMiddleware } from '@clerk/express'
+
+const app = express();
+const port = 5000;
+
+//Middleware
+app.use(cors());
+app.use(express.json({limit: "20mb"}));
+app.use(express.urlencoded({limit: "20mb", extended: true}));
+app.use(clerkMiddleware())
+
+//DB
+
+
+//Routes
+
+app.get('/',( req, res ) => {
+    res.send("API is working");
+});
+
+app.listen(5000, () => {
+  console.log("Server started at http://localhost:5000");
+});
