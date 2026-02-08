df08b2e85c69862e6911fbca24789fc9e9d00448
Sweta Dahal <dahalsweta5@gmail.com>
2026-02-08 21:05:43 +0545

    SignIn and SignUp using Clerk authentication

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,10 +1,57 @@
-import React from 'react'
-import { Link } from "react-router-dom";
+import React, { useState } from 'react'
+import { Link, useNavigate } from "react-router-dom";
 
 import {navbarStyles} from '../assets/dummyStyles';
 import logo from '../assets/transparentLogo.png'
+import { useUser } from '@clerk/clerk-react'; 
+import { useRef } from 'react';
+import { useAuth, useClerk } from '@clerk/clerk-react';
+import { SignedOut } from '@clerk/clerk-react';
 
 const Navbar = () => {
+
+    const [open, setOpen] = useState(false);
+    const [profileOpen, setProfileOpen] = useState(false);
+
+    const {user} = useUser();
+    const { getToken, isSignedIn } = useAuth();
+    const clerk = useClerk();
+
+    const navigate = useNavigate();
+    const profileRef = useRef(null);
+    const TOKEN_KEY = "token";
+
+    // to open login modal
+    function openSignIn(){
+        try{
+            if(clerk && typeof clerk.openSignIn === "function"){
+                clerk.openSignIn();
+            } else {
+                navigate("/login");
+            }
+        }
+        catch (error) {
+            console.error("Open Signin failed:", e);
+            navigate("/login");
+        }
+    }
+
+    
+    // to open signup modal
+    function openSignUp(){
+        try{
+            if(clerk && typeof clerk.openSignUp === "function"){
+                clerk.openSignUp();
+            } else {
+                navigate("/signup");
+            }
+        }
+        catch (error) {
+            console.error("Open Signup failed:", e);
+            navigate("/signup");
+        }
+    }
+
   return (
     <header className={navbarStyles.header}>
         <div className={navbarStyles.container}>
@@ -15,6 +62,40 @@ const Navbar = () => {
                         <span className={navbarStyles.logoText}>Invoura</span>
 
                     </Link>
+                    <div className={navbarStyles.desktopNav}>
+                        <a href="#features" className={navbarStyles.navLink}>
+                            Features
+                        </a>
+                        <a href="#pricing" className={navbarStyles.navLinkInactive}>
+                            Pricing
+                        </a>
+                    </div>
+                </div>
+
+                <div className='flex items-center gap-4'>
+                    <div className={navbarStyles.authSection}>
+                    <SignedOut>
+                        <button 
+                            onClick = {openSignIn} 
+                            className={navbarStyles.signInButton} 
+                            type='button'
+                        >
+                         Sign in
+                        </button>
+
+                        <button 
+                            onClick={openSignUp} 
+                            className={navbarStyles.signUpButton} 
+                            type='button'
+                        >
+                            <div className={navbarStyles.signUpOverlay}></div>
+                            <span className={navbarStyles.signUpText}>
+                                Get Started
+                            </span>
+                         
+                        </button>
+                    </SignedOut>
+                    </div>
                 </div>
             </nav>
         </div>
