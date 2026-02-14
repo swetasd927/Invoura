ddef80baa47787c447fecfca8dfaab63b15501b4
Sweta Dahal <dahalsweta5@gmail.com>
2026-02-14 11:17:55 +0545

    Commit: App.jsx setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,37 @@
 import React from 'react';
-import {Routes, Route } from 'react-router-dom'
+import {Routes, Route } from 'react-router-dom';
 import Home from './pages/Home.jsx';
+import { RedirectToSignIn, SignedIn, SignedOut } from '@clerk/clerk-react';
+import AppShell from './components/AppShell.jsx';
+import Dashboard from './pages/Dashboard.jsx';
+
+const ClerkProtected = ({children}) => (
+  <>
+  <SignedIn>{children}</SignedIn>
+  <SignedOut>
+    <RedirectToSignIn />
+  </SignedOut>
+  </>
+);
 
 const App = () => {
   return (
-    <Routes>
+    <div className='min-h-screen max-w-full overflow-x-hidden'>
+      <Routes>
       <Route path = '/' element = {<Home />} />
+      {/* it must be a protected route */}
+      <Route path = "/app" 
+      element = {
+      <ClerkProtected>
+        <AppShell />
+      </ClerkProtected>
+      }
+      >
+        <Route index element = {<Dashboard/>}/>
+        <Route path = "dashboard" element= {<Dashboard />} />
+      </Route>
     </Routes>
+    </div>
   );
 };
 
