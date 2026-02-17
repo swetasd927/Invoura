8478f1116f11a2f4c71bbc7cbdb5f57b078a7bc6
Sweta Dahal <dahalsweta5@gmail.com>
2026-02-17 21:16:18 +0545

    Feature section completed

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from '../components/Navbar.jsx'
 import Hero from '../components/Hero.jsx'
+import Features from '../components/Features.jsx'
 
 const Home = () => {
   return (
@@ -8,6 +9,9 @@ const Home = () => {
         <Navbar />
         <main className=''>
           <Hero />
+          <div className=''>
+          <Features />
+          </div>
         </main>
     </div>
   )
