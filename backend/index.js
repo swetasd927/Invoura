4ffc584da01a309d9f886aa58be65bfbdfc9ceab
Sweta Dahal <dahalsweta5@gmail.com>
2026-04-28 10:28:36 +0545

    fix: main server file

--- backend/index.js
+++ backend/index.js
@@ -37,6 +37,9 @@ app.get('/',( req, res ) => {
     res.send("API is working");
 });
 
-app.listen(port, () => {
-  console.log(`Server started at http://localhost:${port}`);
-}); 
+// app.listen(port, () => {
+//   console.log(`Server started at http://localhost:${port}`);
+// }); 
+
+//for vercel prod
+export default app;
