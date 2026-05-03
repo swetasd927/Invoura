0e1894487951f6e59f9189e8d3b8195d8c3a9531
Sweta Dahal <dahalsweta5@gmail.com>
2026-05-04 00:06:19 +0545

    Fix: server connection

--- backend/index.js
+++ backend/index.js
@@ -34,7 +34,7 @@ app.use(cors({
         
         const allowedOrigins = [
             process.env.CLIENT_URL || "http://localhost:5173",
-            "http://localhost:3000",
+            "http://localhost:5000",
             "https://invoura.vercel.app",
             "https://invouraa.vercel.app",
             "https://invourafe.vercel.app",
